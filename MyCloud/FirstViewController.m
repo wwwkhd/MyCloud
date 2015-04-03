97b7a2a0067ae491c8443d0709700ef9a5e33444
wwwkhd <wwwkhd@163.com>
2015-04-03 14:30:02 +0800

    111

--- MyCloud/FirstViewController.m
+++ MyCloud/FirstViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     UILabel *lable = [[UILabel alloc] init];
     [self.view addSubview:lable];
+    // test in 2014/12/20   
 }
 
 - (void)didReceiveMemoryWarning {
